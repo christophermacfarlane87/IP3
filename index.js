544cb2b1c7d27904df3532072fe11a84e40bce58
christophermacfarlane87 <cmacfa302@gmail.com>
2024-03-16 12:42:44 +0000

    adding different pages

--- index.js
+++ index.js
@@ -16,8 +16,45 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index', {  }); 
 });
-
-// Routing 
+app.get('/fresh meat', (req, res) => {
+  res.render('freshMeat', {  }); 
+});
+app.get('/fish', (req, res) => {
+  res.render('fish', {  }); 
+});
+app.get('/frozen', (req, res) => {
+  res.render('frozen', {  }); 
+});
+app.get('/dairy', (req, res) => {
+  res.render('dairy', {  }); 
+});
+app.get('/fruit', (req, res) => {
+  res.render('fruitVeg', {  }); 
+});
+app.get('/veg', (req, res) => {
+  res.render('fruitVeg', {  }); 
+});
+app.get('/drystore', (req, res) => {
+  res.render('drystore', {  }); 
+});
+app.get('/desserts', (req, res) => {
+  res.render('desserts', {  }); 
+});
+app.get('/bakery', (req, res) => {
+  res.render('bakery', {  }); 
+});
+app.get('/stock', (req, res) => {
+  res.render('stock', {  }); 
+});
+app.get('/sales', (req, res) => {
+  res.render('sales', {  }); 
+});
+app.get('/order', (req, res) => {
+  res.render('orders', {  }); 
+});
+app.get('/menu', (req, res) => {
+  res.render('menu', {  }); 
+});
 app.get('/Login', (req, res) => {
   res.render('Login', { pageTitle: 'Staff Login Page' }); 
 });
